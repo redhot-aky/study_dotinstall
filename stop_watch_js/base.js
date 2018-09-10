28301ba0f519938ac1ca9a5cdc1b2cd8aca4bc98
Akihiro Watanabe <redhot.aky@gmail.com>
2018-09-10 20:12:43 +0900

    first commit

---
+++ stop_watch_js/base.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  var timer = document.getElementById('timer');
+  var start = document.getElementById('start');
+  var stop = document.getElementById('stop');
+  var reset = document.getElementById('reset');
+
+  var startTime;
+  var elapsedTime = 0;
+  var timeToAdd = 0;
+  var timerId;
+
+  function updateTimeText() {
+   var m =  Math.floor(elapsedTime / 60000);
+   var s = Math.floor(elapsedTime % 60000 / 1000);
+   var ms = elapsedTime % 1000;
+   
+   m = ('0' + m).slice(-2);
+   s = ('0' + s).slice(-2);
+   ms = ('00' + ms).slice(-3);
+
+   timer.textContent = m + ':' + s + '.' + ms;
+  }
+
+  function countUp() {
+    timerId = setTimeout(function() {
+      elapsedTime = Date.now() - startTime + timeToAdd;
+      updateTimeText();
+      countUp();
+    }, 10);
+  }
+
+  start.addEventListener('click', function(){
+    startTime = Date.now();
+    countUp();
+  });
+
+  stop.addEventListener('click', function(){
+    clearTimeout(timerId);
+    timeToAdd += Date.now() - startTime;
+  });
+
+  reset.addEventListener('click', function(){
+    elapsedTime = 0;
+    timeToAdd = 0;
+    updateTimeText();
+  });
+
+})();
