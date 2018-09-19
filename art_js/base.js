844ceee386efc4b882df2e8d0d276a0f176e54e5
Akihiro Watanabe <redhot.aky@gmail.com>
2018-09-19 21:03:38 +0900

    art_js　完成

--- art_js/base.js
+++ art_js/base.js
@@ -4,6 +4,9 @@
   var canvas;
   var ctx;
   var Ball;
+  var balls = [];
+  var Stage;
+  var stage;
 
   canvas = document.getElementById('myCanvas');
   if(!canvas || !canvas.getContext) return false;
@@ -13,20 +16,84 @@
     return min + Math.floor(Math.random() * (max - min + 1));
   }
 
+  function adjustPosition(pos, r, max) {
+    if(pos - r < 0) {
+      return r;
+    } else if (pos + r > max) {
+      return max - r;
+    } else {
+      return pos;
+    }
+  }
+
+  canvas.addEventListener('click', function(e) {
+    var x, y, r;
+    var rect;
+    // x = rand(r, canvas.width - r);
+    // y = rand(r, canvas.height - r);
+    rect = e.target.getBoundingClientRect();
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    r = rand(0, 100) < 20 ? rand(50, 100): rand(10, 35);
+
+    // if(x - r < 0) x = r;
+    // if(y - r < 0) y = r;
+    // if(x + r > canvas.width) x = canvas.width - r;
+    // if(y + r > canvas.height) y = canvas.height - r;
+
+    x = adjustPosition(x, r, canvas.width);
+    y = adjustPosition(y, r, canvas.height);
+
+    balls.push(new Ball(x, y, r));
+  });
+
   Ball = function(x, y ,r) {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.color = 'hsla(120, 80%, 40%, 0.8)';
-    this.draw = function() {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
-      ctx.fillStyle = this.color;
-      ctx.closePath();
-      ctx.fill();
-    };
+    this.vx = rand(-10, 10);
+    this.vy = rand(-10, 10);
+    this.color = 'rgba('+ rand(0,255) + ', ' + rand(0,255) + ', ' + rand(0,255) + ', ' + Math.random() + ')';
   };
 
-  var ball = new Ball(rand(100,200), rand(100,200), rand(10, 50));
-  ball.draw();
+  Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+    ctx.fillStyle = this.color;
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  Ball.prototype.move = function() {
+    if(this.x + this.r > canvas.width || this.x - this.r < 0) {
+      this.vx *= -1;
+    }
+    if(this.y + this.r > canvas.height || this.y - this.r < 0) {
+      this.vy *= -1;
+    }
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  Stage = function() {
+    this.update = function() {
+      var i;
+      this.clear();
+      for(i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].move();
+      }
+      setTimeout(function() {
+        this.update();
+      }.bind(this),30);
+    }
+    this.clear = function() {
+      ctx.fillStyle = '#ffffff';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+  }
+
+  stage = new Stage();
+  stage.update();
+
 })();
